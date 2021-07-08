c262ab2b3a58872b55b4bdf3375b091cbdcd9c48
kgentel <katie.m.gentel@gmail.com>
2021-07-08 16:45:58 -0500

    added tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,7 +4,6 @@ const { Category, Product } = require("../../models");
 // The `/api/categories` endpoint
 
 router.get("/", (req, res) => {
-  // find all categories
   // be sure to include its associated Products
   Category.findAll({
     attributes: ["id", "category_name"],
